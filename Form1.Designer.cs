15f4fe4ed3fad00283646f00009c8c47e99275b9
JhonyJT <jhonytaipe2001@gmail.com>
2024-08-12 00:56:16 -0500

    upload first

---
+++ Form1.Designer.cs
@@ -0,0 +1,147 @@
+﻿namespace PICO_PLACAS
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtFecha = new System.Windows.Forms.TextBox();
+            this.txtHora = new System.Windows.Forms.TextBox();
+            this.txtPlaca = new System.Windows.Forms.TextBox();
+            this.btnConsultar = new System.Windows.Forms.Button();
+            this.lblResultado = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(214, 72);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(58, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Fecha:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(214, 120);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(48, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Hora:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(214, 172);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(52, 20);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Placa:";
+            // 
+            // txtFecha
+            // 
+            this.txtFecha.Location = new System.Drawing.Point(360, 66);
+            this.txtFecha.Name = "txtFecha";
+            this.txtFecha.Size = new System.Drawing.Size(190, 26);
+            this.txtFecha.TabIndex = 3;
+            this.txtFecha.Text = "AAAA-MM-DD";
+            this.txtFecha.Enter += new System.EventHandler(this.fechaEntrar);
+            this.txtFecha.Leave += new System.EventHandler(this.fechaSalir);
+            // 
+            // txtHora
+            // 
+            this.txtHora.Location = new System.Drawing.Point(360, 114);
+            this.txtHora.Name = "txtHora";
+            this.txtHora.Size = new System.Drawing.Size(190, 26);
+            this.txtHora.TabIndex = 4;
+            this.txtHora.Text = "HH:MM";
+            this.txtHora.Enter += new System.EventHandler(this.horaEnter);
+            this.txtHora.Leave += new System.EventHandler(this.horaSalir);
+            // 
+            // txtPlaca
+            // 
+            this.txtPlaca.Location = new System.Drawing.Point(360, 166);
+            this.txtPlaca.Name = "txtPlaca";
+            this.txtPlaca.Size = new System.Drawing.Size(190, 26);
+            this.txtPlaca.TabIndex = 5;
+            this.txtPlaca.Text = "AAA - 0123";
+            this.txtPlaca.Enter += new System.EventHandler(this.placaEntrar);
+            this.txtPlaca.Leave += new System.EventHandler(this.placaSalir);
+            // 
+            // btnConsultar
+            // 
+            this.btnConsultar.Location = new System.Drawing.Point(334, 275);
+            this.btnConsultar.Name = "btnConsultar";
+            this.btnConsultar.Size = new System.Drawing.Size(116, 45);
+            this.btnConsultar.TabIndex = 6;
+            this.btnConsultar.Text = "Consultar";
+            this.btnConsultar.UseVisualStyleBackColor = true;
+            this.btnConsultar.Click += new System.EventHandler(this.btnConsultar_Click);
+            // 
+            // lblResultado
+            // 
+            this.lblResultado.Location = new System.Drawing.Point(218, 344);
+            this.lblResultado.Name = "lblResultado";
+            this.lblResultado.Size = new System.Drawing.Size(356, 26);
+            this.lblResultado.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblResultado);
+            this.Controls.Add(this.btnConsultar);
+            this.Controls.Add(this.txtPlaca);
+            this.Controls.Add(this.txtHora);
+            this.Controls.Add(this.txtFecha);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Consulta Pico y Placa";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtFecha;
+        private System.Windows.Forms.TextBox txtHora;
+        private System.Windows.Forms.TextBox txtPlaca;
+        private System.Windows.Forms.Button btnConsultar;
+        private System.Windows.Forms.TextBox lblResultado;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PICO_PLACAS
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+      
+        private void fechaSalir(object sender, EventArgs e)
+        {
+         if (txtFecha.Text == "") {
+             txtFecha.Text = "AAAA-MM-DD";
+
+             txtFecha.ForeColor = Color.Silver;
+            }
+        }
+
+        private void fechaEntrar(object sender, EventArgs e)
+        {
+         if (txtFecha.Text == "AAAA-MM-DD") {
+             txtFecha.Text = "";
+
+             txtFecha.ForeColor = Color.Black;
+            }
+        }
+
+        private void horaEnter(object sender, EventArgs e)
+        {
+            if (txtHora.Text == "HH:MM")
+            {
+                txtHora.Text = "";
+
+                txtHora.ForeColor = Color.Black;
+            }
+        }
+
+        private void horaSalir(object sender, EventArgs e)
+        {
+            if (txtHora.Text == "")
+            {
+                txtHora.Text = "HH:MM";
+
+                txtHora.ForeColor = Color.Silver;
+            }
+        }
+
+        private void placaEntrar(object sender, EventArgs e)
+        {
+            if (txtPlaca.Text == "AAA - 0123")
+            {
+                txtPlaca.Text = "";
+
+                txtPlaca.ForeColor = Color.Black;
+            }
+        }
+
+        private void placaSalir(object sender, EventArgs e)
+        {
+            if (txtPlaca.Text == "")
+            {
+                txtPlaca.Text = "AAA - 0123";
+
+                txtPlaca.ForeColor = Color.Silver;
+            }
+        }
+
+        private void btnConsultar_Click(object sender, EventArgs e)
+        {
+            string connectionString = "Server=LAPTOP-DKTPKMQ6;Database=PicoPlacaDB;Trusted_Connection=True;";
+
+            // Obtener los valores ingresados por el usuario
+            string fechaInput = txtFecha.Text;
+            string horaInput = txtHora.Text;
+            string placa = txtPlaca.Text;
+
+            DateTime fecha;
+            TimeSpan hora;
+
+            // Validar las entradas
+            if (!DateTime.TryParseExact(fechaInput, "yyyy-MM-dd", CultureInfo.InvariantCulture, DateTimeStyles.None, out fecha))
+            {
+                MessageBox.Show("Fecha no válida.");
+                return;
+            }
+
+            if (!TimeSpan.TryParse(horaInput, out hora))
+            {
+                MessageBox.Show("Hora no válida.");
+                return;
+            }
+
+            if (placa.Length == 0 || !char.IsDigit(placa[placa.Length - 1]))
+            {
+                MessageBox.Show("Número de placa no válido.");
+                return;
+            }
+
+            char ultimoDigito = placa[placa.Length - 1];
+            int digito = int.Parse(ultimoDigito.ToString());
+
+            // Reglas de Pico y Placa en Quito
+            var restricciones = new (DayOfWeek dia, int[] digitos)[]
+            {
+                (DayOfWeek.Monday, new int[] { 1, 2 }),
+                (DayOfWeek.Tuesday, new int[] { 3, 4 }),
+                (DayOfWeek.Wednesday, new int[] { 5, 6 }),
+                (DayOfWeek.Thursday, new int[] { 7, 8 }),
+                (DayOfWeek.Friday, new int[] { 9, 0 })
+            };
+
+            TimeSpan inicioManana = new TimeSpan(7, 0, 0);
+            TimeSpan finManana = new TimeSpan(9, 30, 0);
+            TimeSpan inicioTarde = new TimeSpan(16, 0, 0);
+            TimeSpan finTarde = new TimeSpan(19, 30, 0);
+
+            bool puedeCircular = true;
+            string mensaje = "El vehículo PUEDE circular en la fecha y hora especificadas.";
+
+            foreach (var restriccion in restricciones)
+            {
+                if (fecha.DayOfWeek == restriccion.dia && Array.Exists(restriccion.digitos, d => d == digito))
+                {
+                    if ((hora >= inicioManana && hora <= finManana) || (hora >= inicioTarde && hora <= finTarde))
+                    {
+                        puedeCircular = false;
+                        mensaje = "El vehículo NO puede circular en la fecha y hora especificadas.";
+                        break;
+                    }
+                }
+            }
+
+            // Mostrar el resultado en la interfaz
+            lblResultado.Text = mensaje;
+
+            // Guardar la información en la base de datos
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                string query = "INSERT INTO Consultas (FechaConsulta, FechaIngresada, HoraIngresada, NumeroPlaca, PuedeCircular, Mensaje) " +
+                               "VALUES (@FechaConsulta, @FechaIngresada, @HoraIngresada, @NumeroPlaca, @PuedeCircular, @Mensaje)";
+
+                using (SqlCommand command = new SqlCommand(query, connection))
+                {
+                    command.Parameters.AddWithValue("@FechaConsulta", DateTime.Now);
+                    command.Parameters.AddWithValue("@FechaIngresada", fecha);
+                    command.Parameters.AddWithValue("@HoraIngresada", hora);
+                    command.Parameters.AddWithValue("@NumeroPlaca", placa);
+                    command.Parameters.AddWithValue("@PuedeCircular", puedeCircular);
+                    command.Parameters.AddWithValue("@Mensaje", mensaje);
+
+                    command.ExecuteNonQuery();
+                }
+            }
+        }
+    }
+}
